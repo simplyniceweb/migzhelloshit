dd6b76ccc1eed84e30eeb5608bf927016a8ff541
Jaylord Ferrer <simplyniceweb@gmail.com>
2017-10-04 12:50:56 +0800

    shit

--- app/src/main/java/com/example/hp/guessimage/MainActivity.java
+++ app/src/main/java/com/example/hp/guessimage/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,11 +17,11 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-
     private ImageView iv;
     private Button b1;
     private Button b2;
-
+    private Button b3;
+    String Answer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,29 +36,51 @@ public class MainActivity extends AppCompatActivity {
         iv = (ImageView) findViewById(R.id.imageView);
         b1 = (Button) findViewById(R.id.b1);
         b2 = (Button) findViewById(R.id.b2);
+        b3 = (Button) findViewById(R.id.b3);
 
-        final Random       random    = new Random();
-        final List<String> keys      = new ArrayList<>(flags.keySet());
-        String       randomKey = keys.get( random.nextInt(keys.size()) );
-        Integer       value     = (Integer) flags.get(randomKey);
+        final Random random = new Random();
+        final List<String> keys = new ArrayList<>(flags.keySet());
+        String randomKey = keys.get( random.nextInt(keys.size()) );
+        Integer value = (Integer) flags.get(randomKey);
 
+        setText(randomKey, value);
+        optionListener(keys, random, flags);
+        setRandomOptions();
+    }
+
+    public void setText(String randomKey, Integer value) {
         b1.setText(randomKey);
-        b2.setText("Jaylord");
         iv.setImageResource(value);
+    }
 
-
+    public void optionListener(final List<String> keys, final Random random, final Map flags) {
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String       randomKeyClick = keys.get( random.nextInt(keys.size()) );
-                Integer       valueClick     = (Integer) flags.get(randomKeyClick);
+                String randomKeyClick = keys.get( random.nextInt(keys.size()) );
+                Integer valueClick = (Integer) flags.get(randomKeyClick);
 
-                b1.setText(randomKeyClick);
-                b2.setText("Jaylord");
-                iv.setImageResource(valueClick);
+                setText(randomKeyClick, valueClick);
+                setRandomOptions();
             }
         });
-
-
     }
+
+    public void setRandomOptions() {
+        List<String> answers = new ArrayList<>();
+        String[] options = {"PH", "US", "NK", "SK"};
+
+        Integer opt = 0;
+        while (2 > opt) {
+            String randomOption = options[new Random().nextInt(options.length)];
+            if (!answers.contains(randomOption)) {
+                answers.add(randomOption);
+                opt++;
+            }
+        }
+
+        b2.setText(answers.get(0));
+        b3.setText(answers.get(1));
+    }
+
 }
